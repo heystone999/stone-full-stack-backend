74d63ff032431cd35e5b4ddd36cbd49ee71975b7
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-05 18:43:09 -0500

    feat: Exercises 3.3

--- index.js
+++ index.js
@@ -43,6 +43,15 @@ app.get('/info', (request, response) => {
   response.send(infoMessage)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
 
 
 const PORT = 3001
