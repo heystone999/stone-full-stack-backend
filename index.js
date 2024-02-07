eaf6f2eba81c7e458cb70c85547baa52a427be58
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-07 18:13:41 -0500

    feat: Exercises 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -12,6 +13,7 @@ morgan.token('postData', (request, response) => {
 })
 
 
+app.use(cors())
 app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
@@ -98,6 +100,6 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`);
