22eaf7cec8de1af5a1453af7358007f2b7f357ff
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-08 18:26:32 -0500

    feat: Exercises 3.16

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const Person = require('./models/person')
 
 const app = express()
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
 
 morgan.token('postData', (request, response) => {
   if (request.method === 'POST') {
@@ -21,6 +29,7 @@ app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 app.use(express.static('dist'))
+app.use(errorHandler)
 
 
 
@@ -44,10 +53,16 @@ app.get('/info', (request, response) => {
   response.send(infoMessage)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -55,10 +70,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .then(result => {
       response.status(204).end()
     })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
