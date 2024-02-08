bd3c5904ebe4907827d6e2ff99b8565c20c1ecd2
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-08 17:09:00 -0500

    refacter: database configuration into its own module: person.js

--- index.js
+++ index.js
@@ -2,36 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-
-const url = process.env.MONGODB_URL
-mongoose.set('strictQuery', false)
-
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('Error: connecting to MongoDB:', error.message)
-  })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-
-
-const Person = mongoose.model('Person', personSchema)
+const Person = require('./models/person')
 
 
 const app = express()
