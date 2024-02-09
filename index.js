c51e4e2baa2890f6ab4635bd49165e54e5cb37a2
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-09 00:03:49 -0500

    feat: Exercises 3.19 backend

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
@@ -29,7 +31,6 @@ app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
 app.use(express.static('dist'))
-app.use(errorHandler)
 
 
 
@@ -74,7 +75,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -90,18 +91,17 @@ app.post('/api/persons', (request, response) => {
     name: body.name,
     number: body.number
   })
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -109,6 +109,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 
+app.use(errorHandler)
 
 
 
